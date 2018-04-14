589670def9b18390d27e36891d830c4e6a07aa42
sj <sebastianthegreatful@gmail.com>
2018-04-14 14:49:31 +0200

    Add endpoint with some whoami data

--- src/main/kotlin/dk/fitfit/whoami/SpringWhoamiApplication.kt
+++ src/main/kotlin/dk/fitfit/whoami/SpringWhoamiApplication.kt
@@ -2,10 +2,28 @@ package dk.fitfit.whoami
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.http.HttpHeaders
+import java.net.InetAddress
 
 @SpringBootApplication
 class SpringWhoamiApplication
 
 fun main(args: Array<String>) {
     runApplication<SpringWhoamiApplication>(*args)
+}
+
+@RestController
+class WhoamiController {
+    @GetMapping("/")
+    fun whoami(@RequestHeader headers: HttpHeaders): Map<String, Any>  {
+        val hostname = InetAddress.getLocalHost().getHostName()
+        return return mapOf(
+                "hostname" to hostname,
+                "headers" to headers,
+                "ip" to InetAddress.getLocalHost()
+            )
+    }
 }
