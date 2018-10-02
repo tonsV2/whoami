e6bce64a5798490aaa8fbc5eef1dab1dec1bc943
sj <sebastianthegreatful@gmail.com>
2018-10-02 12:58:24 +0200

    Remove method used for testing of exception handling

--- src/main/kotlin/dk/fitfit/whoami/SpringWhoamiApplication.kt
+++ src/main/kotlin/dk/fitfit/whoami/SpringWhoamiApplication.kt
@@ -6,7 +6,6 @@ import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.net.InetAddress
-import javax.servlet.http.HttpServletRequest
 
 @SpringBootApplication
 class SpringWhoamiApplication
@@ -23,11 +22,5 @@ class WhoamiController {
     fun whoami(): String {
         logger.info("Whoami request");
         return InetAddress.getLocalHost().getHostName()
-    }
-
-    @GetMapping("/exception")
-    fun whoami(request: HttpServletRequest): String {
-        logger.info("Whoami exception");
-        throw Exception("shalalala except!")
     }
 }
